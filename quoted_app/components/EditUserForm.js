cd03123b69579f280c5b01a59468fb89c566ed61
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-29 19:17:24 -0400

    fix bug that made plus icon block clicks to file input for profile picture

--- quoted_app/components/EditUserForm.js
+++ quoted_app/components/EditUserForm.js
@@ -44,15 +44,15 @@ export default function EditUserForm({ closeEditModal, userData }) {
     e.preventDefault();
 
     try {
+      let newProfilePicture = fireStoreImage;
+      if (imageUpload) {
+        newProfilePicture = await uploadImage();
+      }
+
       await updateDoc(doc(db, "users", currentUser.uid), {
         userName: username,
         email: email,
-      });
-
-      await uploadImage();
-
-      await updateDoc(doc(db, "users", currentUser.uid), {
-        profilePicture: fireStoreImage,
+        profilePicture: newProfilePicture,
       });
 
       closeEditModal();
@@ -63,7 +63,7 @@ export default function EditUserForm({ closeEditModal, userData }) {
     }
   };
 
-  const uploadImage = async (e) => {
+  const uploadImage = async () => {
     if (!imageUpload) return;
 
     const imageRef = ref(storage, `images/${currentUser.uid}`);
@@ -76,19 +76,18 @@ export default function EditUserForm({ closeEditModal, userData }) {
       setFireStoreImage(downloadURL);
 
       console.log("Image uploaded successfully");
-      alert("Image uploaded successfully");
+      // alert("Image uploaded successfully");
+
+      return downloadURL;
     } catch (error) {
       console.log("Error uploading image:", error);
-      alert("Error uploading image");
+      // alert("Error uploading image");
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col justify-center">
       <div className="mb-4">
-        <label htmlFor="profilePictureFile" className="block mb-1">
-          Profile Picture
-        </label>
         <div
           style={{
             backgroundImage: `url(${fireStoreImage})`,
@@ -99,21 +98,26 @@ export default function EditUserForm({ closeEditModal, userData }) {
           }}
           className="border border-gray-300 rounded px-2 py-1 h-[6rem] w-[6rem] text-center flex items-center justify-center"
         >
-          <input
-            type="file"
-            id="profilePictureFile"
-            style={{
-              opacity: 0,
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-            }}
-            className="border border-gray-300 rounded"
-            onChange={(e) => setImageUpload(e.target.files[0])}
-          ></input>
-          <i class="fa-solid fa-plus my-auto text-5xl text-white opacity-60 "></i>
+          <label htmlFor="profilePictureInput" className="cursor-pointer">
+            <input
+              type="file"
+              id="profilePictureInput"
+              style={{
+                opacity: 0,
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+              }}
+              className="border border-gray-300 rounded"
+              onChange={(e) => {
+                setFireStoreImage(URL.createObjectURL(e.target.files[0]));
+                setImageUpload(e.target.files[0]);
+              }}
+            />
+            <i className="fa-solid fa-plus my-auto text-5xl text-white opacity-60"></i>
+          </label>
         </div>
       </div>
       <div className="mb-4">
@@ -142,20 +146,6 @@ export default function EditUserForm({ closeEditModal, userData }) {
           required
         />
       </div>
-      {/* <div className="mb-4">
-        <label htmlFor="profilePicture" className="block mb-1">
-          Profile Picture URL:
-        </label>
-        <input
-          type="text"
-          id="profilePicture"
-          value={profilePicture || genericProfilePicture}
-          onChange={(e) => setProfilePicture(e.target.value)}
-          className="border border-gray-300 rounded px-2 py-1 w-full"
-          required
-        />
-      </div> */}
-
       <button
         type="submit"
         className="w-full bg-light hover:bg-opacity-90 text-white py-2 px-4 rounded transition-colors duration-300"
