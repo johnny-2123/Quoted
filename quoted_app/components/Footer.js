6907886991112b89fc7796197c3d6d0fbb0d3c11
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-30 18:12:27 -0400

    add coming soon messages to friends, favorites, and liked icons

--- quoted_app/components/Footer.js
+++ quoted_app/components/Footer.js
@@ -1,27 +1,37 @@
 import React from "react";
 import Link from "next/link";
 import { useAuth } from "@/context/AuthContext";
+import { Slide, toast } from "react-toastify";
 
 export default function Footer() {
   const { currentUser } = useAuth();
 
   if (!currentUser) return null;
 
+  const notifyComingSoon = () => {
+    toast.info("Feature coming soon!", {
+      transition: Slide,
+      autoClose: 2000,
+      hideProgressBar: true,
+    });
+  };
+
   return (
     <div className="flex justify-between items-center gap-8 py-3 px-[1.5rem] mx-0 text-2xl xxs:px-[1rem] xxs:gap-2 xxs:justify-between">
-      <Link href={"/friends"}>
-        <i className="fa-solid fa-users duration-300 hover:opacity-60 cursor-pointer text-light"></i>
+      <Link href={""} onClick={notifyComingSoon}>
+        <i className="fa-solid fa-users duration-300 hover:none cursor-not-allowed text-light"></i>
       </Link>
-      <Link href={"/likes"}>
-        <i className="fa-regular fa-heart duration-300 hover:opacity-60 cursor-pointer text-light"></i>
+      <Link href={""} onClick={notifyComingSoon}>
+        <i className="fa-regular fa-heart duration-300 hover:opacity-60 cursor-not-allowed text-light"></i>
       </Link>
       <Link href={"/"} className="duration-300 hover:opacity-60 cursor-pointer">
         <i className="fa-solid fa-quote-left   text-light mr-1"></i>
         <i className="fa-solid fa-quote-right  text-light"></i>
       </Link>
       <Link
-        href={"/favorites"}
-        className="duration-300 hover:opacity-60 cursor-pointer text-light"
+        href={""}
+        onClick={notifyComingSoon}
+        className="duration-300 hover:opacity-60 cursor-not-allowed text-light"
       >
         <i className="fa-regular fa-star"></i>
       </Link>
