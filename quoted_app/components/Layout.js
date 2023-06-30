dc5cc1766097875849ddd0b9eb4b5f404f2c531b
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-29 21:33:06 -0400

    use inner height as max height in layout component

--- quoted_app/components/Layout.js
+++ quoted_app/components/Layout.js
@@ -1,13 +1,24 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 
 export default function Layout(props) {
   const { children } = props;
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const updateSize = () => {
+      setHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
   return (
-    <div className="flex flex-col min-h-screen max-h-screen relative bg-background ">
-      <main className="flex-1 flex flex-col overflow-hidden p-4 text-slate-900">
+    <div
+      className="flex flex-col min-h-screen relative bg-background overflow-hidden"
+      style={{ maxHeight: `${height}px` }}
+    >
+      <main className="flex-1 flex flex-col overflow-auto p-4 text-slate-900">
         {children}
       </main>
       <Footer />
