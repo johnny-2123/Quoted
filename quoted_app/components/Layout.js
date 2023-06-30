c75af0bd57eaf4c9a52d34152b53edc3d452dd5f
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-29 21:46:41 -0400

    add max height to tailwind config

--- quoted_app/components/Layout.js
+++ quoted_app/components/Layout.js
@@ -6,25 +6,22 @@ export default function Layout(props) {
   const { children } = props;
 
   useEffect(() => {
-    function updateHeight() {
-      const vh = window.innerHeight * 0.01;
+    const setVh = () => {
+      let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty("--vh", `${vh}px`);
-    }
-
-    window.addEventListener("resize", updateHeight);
-    updateHeight();
-
-    return () => {
-      window.removeEventListener("resize", updateHeight);
     };
+
+    setVh();
+
+    window.addEventListener("resize", setVh);
+
+    return () => window.removeEventListener("resize", setVh);
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen max-h-screen relative bg-background">
-      <main
-        className="flex-1 flex flex-col overflow-hidden p-4 text-slate-900"
-        style={{ maxHeight: "var(--vh)" }}
-      >
+    <div className="flex flex-col min-h-screen max-h-[calc(100*var(--vh))] relative bg-background ">
+      <Header />
+      <main className="flex-1 flex flex-col overflow-auto p-4 text-slate-900">
         {children}
       </main>
       <Footer />
