fea2d560a2ef81ef6bf384853affca14b9765f23
edgar <johnnyavila2021@u.northwestern.edu>
2023-07-03 14:51:13 -0400

    use next/image component to optimize image loading

--- quoted_app/components/QuoteCard.js
+++ quoted_app/components/QuoteCard.js
@@ -96,25 +96,28 @@ export default function QuoteCard({
   return (
     <div
       key={id}
-      className="flex py-4 pl-1 pr-4 border-y border-gray-200 rounded w-full  "
+      className="flex py-4 pl-1 pr-4 border-y border-gray-200 rounded w-full "
     >
       <Link
         href={{
           pathname: `/profile/${authorData?.uid}`,
           query: { id: authorData?.uid },
         }}
-        className="p-0 m-0 mr-3 w-[4rem] h-[4rem] min-w-[35px] min-h-[35px] max-w-[40px] max-h-[40px]"
+        className="p-0 m-0 mr-3 w-[4rem] h-[4rem] min-w-[35px] min-h-[35px] max-w-[40px] max-h-[40px] relative"
       >
-        {/* <Image
-          src={authorData?.profilePicture}
-      className="rounded-full object-cover min-w-[35px] min-h-[35px] max-w-[40px] max-h-[40px]"
+        <Image
+          src={authorData?.profilePicture || defaultProfilePicture}
+          className="rounded-full object-cover min-w-[35px] min-h-[35px] max-w-[40px] max-h-[40px]"
           alt="Profile Picture"
-        /> */}
-        <img
+          width={0}
+          height={0}
+          style={{ width: "100%", height: "auto" }}
+        />
+        {/* <img
           src={`${authorData?.profilePicture || defaultProfilePicture}`}
           className="rounded-full object-cover min-w-[35px] min-h-[35px] max-w-[40px] max-h-[40px]"
           alt="Profile Picture"
-        />
+        /> */}
       </Link>
       <div className="flex flex-col flex-grow text-xs sm:text-md xs:text-lg">
         <p className="font-bold w-full flex justify-between items-center content-center text-sm lg:text-base">
