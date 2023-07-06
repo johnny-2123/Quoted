4b79498e87bb1891c1d4072ec819a488386e346b
edgar <johnnyavila2021@u.northwestern.edu>
2023-07-06 13:09:32 -0400

    make new quote button position responsive for larger screen sizes

--- quoted_app/components/NewQuoteBtn.js
+++ quoted_app/components/NewQuoteBtn.js
@@ -3,7 +3,7 @@ import React from "react";
 const NewQuoteBtn = ({ setOpenModal }) => {
   return (
     <div
-      className="fixed right-[.5rem] bottom-[4rem] flex items-center justify-center bg-transparent text-[3rem] text-dark cursor-pointer duration-300 transform hover:scale-90 rounded-[50%]]"
+      className="fixed right-[1.2rem] bottom-[1.5rem] md:right-[.5rem] md:bottom-[4rem] flex items-center justify-center bg-transparent text-[3rem] text-dark cursor-pointer duration-300 transform hover:scale-90 rounded-[50%]]"
       onClick={() => setOpenModal(true)}
     >
       <i className="fa-solid fa-circle-plus"></i>
